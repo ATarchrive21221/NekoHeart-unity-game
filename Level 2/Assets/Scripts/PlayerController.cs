bdc8eee99fedd5029afaf56283207a2daf99aa24
Kristyn Le <kristynle0608@gmail.com>
2022-04-01 12:17:36 -0300

    Add code to make the cat jump
    also deleted the gems from the hierarchy

--- Level 2/Assets/Scripts/PlayerController.cs
+++ Level 2/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator anim;
+
+    public float speed = 5;
+    public float jumpforce = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        anim.Play("cat_idle");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+            anim.Play("cat_jump");
+        }
     }
 }
