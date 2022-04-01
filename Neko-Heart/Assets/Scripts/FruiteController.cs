738fe11cd319bd47d9e9b744883d038d1195037f
Kristyn Le <kristynle0608@gmail.com>
2022-04-01 19:36:02 -0300

    Add Timer Text
    Change score value from 10 to 0 in FruitController

--- Neko-Heart/Assets/Scripts/FruiteController.cs
+++ Neko-Heart/Assets/Scripts/FruiteController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FruiteController : MonoBehaviour
 {
-    public int score = 10;
+    public int score = 0;
 
     // Start is called before the first frame update
     void Start()
