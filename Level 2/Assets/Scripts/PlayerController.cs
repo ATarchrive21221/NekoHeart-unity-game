a62d0c1387e245d7f1e991bf1c389342fc637666
Kristyn Le <kristynle0608@gmail.com>
2022-04-01 13:04:35 -0300

    Make the Cat Run
    Added animations to it too :3

--- Level 2/Assets/Scripts/PlayerController.cs
+++ Level 2/Assets/Scripts/PlayerController.cs
@@ -6,26 +6,52 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Animator anim;
+    private SpriteRenderer sprite;
 
     public float speed = 5;
     public float jumpforce = 5;
+    private float dirX = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
 
-        anim.Play("cat_idle");
     }
 
     // Update is called once per frame
     void Update()
     {
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * speed, rb.velocity.y);
+
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpforce);
-            anim.Play("cat_jump");
+        }
+
+        Run();
+
+    }
+
+    void Run()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
         }
     }
+
 }
